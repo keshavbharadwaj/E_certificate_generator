f79d5d954b8f4d244adac8983250edd16238d785
vivekadi <vivek.adishesha@gmail.com>
2020-09-11 20:11:25 +0530

    update
    Co-Authored-By: keshavbharadwaj <45592982+keshavbharadwaj@users.noreply.github.com>
    Co-Authored-By: Shankar <bravoshankar@gmail.com>

--- frontend-python/hh.py
+++ frontend-python/hh.py
@@ -5,26 +5,27 @@ import pandas as pd
 import json
 import sys
 
+
 def add_data(path):
     try:
-        login_url='https://rnsit-ecert.herokuapp.com/users/login'
-        login_data={
-                    "userName":"admin",
-                    "password":"password"
+        login_url = 'https://rnsit-ecert.herokuapp.com/users/login'
+        login_data = {
+            "userName": "admin",
+            "password": "password"
         }
         session = requests.session()
 
-        r = session.post(login_url,data=login_data)
+        r = session.post(login_url, data=login_data)
         user_token = (json.loads(r.text))['data']['userToken']
         print(user_token)
 
         datagram = pd.read_csv(path)
-        datagram['verifyUrl']=np.nan
+        datagram['verifyUrl'] = np.nan
 
-        datagram.drop(datagram.index,inplace=True)
+        datagram.drop(datagram.index, inplace=True)
         print('here1')
-        #print(r.text)
-        #print(user_token)
+        # print(r.text)
+        # print(user_token)
         file_data = {
             'file': (path, open(path, 'rb'))
         }
@@ -36,18 +37,17 @@ def add_data(path):
         print(response.text)
         print("here")
         for i in json.loads(response.text)['data']['result']:
-
             k = pd.DataFrame(i, index=[0])
-            datagram=datagram.append(k)
-           # datagram.append(k)
-            #datagram.append(i)
+            datagram = datagram.append(k)
+        # datagram.append(k)
+        # datagram.append(i)
 
         logout_url = 'https://rnsit-ecert.herokuapp.com/users/logout/' + user_token
         logout_response = requests.get(logout_url)
         print(datagram)
-        #print(logout_response.text)
-        #datagram['serial']=l
-        datagram.rename(columns={'verifyUrl':'serial'},inplace=True)
+        # print(logout_response.text)
+        # datagram['serial']=l
+        datagram.rename(columns={'verifyUrl': 'serial'}, inplace=True)
         datagram.to_csv(path)
     except:
         print("Unexpected error:", sys.exc_info()[0])
@@ -55,4 +55,6 @@ def add_data(path):
         logout_url = 'https://rnsit-ecert.herokuapp.com/users/logout/' + user_token
         logout_response = requests.get(logout_url)
         print(logout_response.text)
-add_data('C://Users//vivek//Desktop//ncetest3//csv//webinar_4_r&d_org.csv')
+
+
+add_data('C://Users//vivek//Desktop//ncetest3//csv//bgp2.csv')
